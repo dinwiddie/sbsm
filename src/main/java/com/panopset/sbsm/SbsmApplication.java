ba25b2648cf989e9108537b650eefe710c745f79
Karl Dinwiddie <karl.dinwiddie@vsp.com>
2025-08-26 06:48:44 -0700

    start.spring.io java 21 maven spring boot 3.5.5

---
+++ src/main/java/com/panopset/sbsm/SbsmApplication.java
@@ -0,0 +1,13 @@
+package com.panopset.sbsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsmApplication.class, args);
+	}
+
+}
